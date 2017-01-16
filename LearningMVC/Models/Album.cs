007e2e31c746a5e32f0702d8ddad02ca88127e5b
cbott28 <cbott28@gmail.com>
2017-01-16 18:13:58 -0500

    Add project files.

---
+++ LearningMVC/Models/Album.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LearningMVC.Models
+{
+    public class Album
+    {
+        public int AlbumID { get; set; }
+
+        [Required()]
+        [Display(Name = "Artist")]
+        public int ArtistID { get; set; }
+
+        [Required()]
+        public string Title { get; set; }
+
+        public Artist Artist { get; set; }
+
+        public List<Review> Reviews { get; set; }
+    }
+}
---
+++ LearningMVC/Models/Review.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LearningMVC.Models
+{
+    public class Review
+    {
+        public int ReviewID { get; set; }
+
+        public int AlbumID { get; set; }
+
+        public Album Album { get; set; }
+
+        public string Content { get; set; }
+
+        [Required()]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email Address")]
+        public string ReviewerEmail { get; set; }
+    }
+}
